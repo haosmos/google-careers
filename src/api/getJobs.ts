e7e1a1af0ae9ef3630a49784ef6284550d2d51a1
chaosmos <vitovich1987@gmail.com>
2023-06-29 14:56:58 +0300

    moved json data to render.com service

--- src/api/getJobs.ts
+++ src/api/getJobs.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-import type { Job } from "@/api/types";
+import type { Job } from '@/api/types';
 
 const getJobs = async () => {
   const baseUrl = import.meta.env.VITE_APP_API_URL;
